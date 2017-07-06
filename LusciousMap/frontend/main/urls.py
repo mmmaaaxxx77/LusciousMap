d2c5a04819b6045407cd79824549bc6529fa5589
mmmaaaxxx77 <mmmaaaxxx77@gmail.com>
2017-07-06 11:27:20 +0800

    no message

--- LusciousMap/frontend/main/urls.py
+++ LusciousMap/frontend/main/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from frontend.main.views import views
 from frontend.main.views import webplugin
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'google83bbd874881151b1.html ^$', TemplateView.as_view(template_name='google/google83bbd874881151b1.html')),
     url(r'^place/new/$', views.new_place, name="new_place"),
     url(r'^place/view/(?P<place_id>[\w\-]+)/$', views.place_view, name="place_view"),
     url(r'^map/view/(?P<map_id>[\w\-]+)/$', views.map_view, name="map_view"),
